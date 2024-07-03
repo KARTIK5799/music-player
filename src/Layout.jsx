432bf4bf8fc23a33c5aefc0401a49e45f2dec658
KARTIK5799 <thekartiksapkal@gmail.com>
2024-07-03 23:33:25 +0530

    Refactor layout with fractional widths for improved responsiveness and alignment

---
+++ src/Layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div className="flex ">
+      <div className="first-section  hidden xl:block w-[20%] p-5 "></div>
+      <div className="second-section  hidden sm:block w-[30%] p-5 overflow-hidden">
+      <div className="bg-gray-300 xl:hidden p-4 h-20 sticky top-0 z-10">
+          Swiggy logo
+        </div>
+        <div className="bg-gray-300 p-4 h-20 sticky top-0 z-10">
+          Header (Sticky)
+        </div>
+        <div className="bg-gray-400 p-4 sticky top-14 h-20 z-10">
+          Search Bar (Sticky)
+        </div>
+        <div className="overflow-y-auto flex-1 p-4 bg-white overflow-scroll">
+          lorem1100
+        </div>
+      </div>
+      <div className="third-section  sm:w-[50%] p-5"></div>
+    </div>
+  );
+};
+
+export default Layout;
